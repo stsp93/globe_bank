d369786b6c3a8578000bbe21a39df74dd28c3d14
stsp93 <stspsp@gmail.com>
2023-06-14 17:44:07 +0300

    added error functions

--- private/functions.php
+++ private/functions.php
@@ -2,4 +2,14 @@
 function url_to($url) {
     return '/public/staff' . $url;
 }
+
+function error_404() {
+    header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
+    exit();
+}
+
+function error_500() {
+    header($_SERVER["SERVER_PROTOCOL"] . " 501 Internal Server Error");
+    exit();
+}
 ?>
